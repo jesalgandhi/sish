63b75be21bde1e18c545a6c3e342af0d0248ba70
Jesal Gandhi <jgandhi5@stevens.edu>
2024-12-15 13:44:41 -0500

    signal handlers

--- sish.c
+++ sish.c
@@ -7,12 +7,21 @@
 
 #include "extern.h"
 
+volatile sig_atomic_t sig_recv = 0;
+
+void
+handle_sig(int sig)
+{
+	(void)sig;
+	sig_recv = 1;
+}
+
 int
 install_handlers()
 {
 	struct sigaction sa;
 
-	sa.sa_handler = SIG_IGN;
+	sa.sa_handler = handle_sig;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
 	if (sigaction(SIGINT, &sa, NULL) == -1) {
@@ -51,13 +60,30 @@ main(int argc, char **argv)
 
 	/* shell mode */
 	while (1) {
+		if (sig_recv) {
+			printf("\n");
+			sig_recv = 0;
+		}
 		printf("sish$ ");
+		fflush(stdout);
+
 
 		nread = getline(&line, &len, stdin);
 		if (nread == -1) {
-			fprintf(stderr, "error reading input or EOF detected\n");
-			return (int)nread;
+			if (feof(stdin)) {
+				fprintf(stderr, "%s: EOF detected. Exiting sish\n",
+				        getprogname());
+				break;
+			}
+			if (errno == EINTR) {
+				continue;
+			} else {
+				fprintf(stderr, "%s: %s\n", getprogname(), strerror(errno));
+				return (int)nread;
+			}
 		}
+
+
 		if (line[nread - 1] == '\n') {
 			line[nread - 1] = '\0';
 		}
@@ -66,5 +92,7 @@ main(int argc, char **argv)
 			break;
 		}
 	}
+
+	free(line);
 	return 0;
 }
